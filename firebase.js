7a25a5ece66b685e6cd491a2f1e245fffaefefeb
Farai Mashonganyika <104810664+rb-mash@users.noreply.github.com>
2025-05-11 03:04:32 +0200

    Create firebase.js
    added new file

---
+++ firebase.js
@@ -0,0 +1,23 @@
+<!-- Navbar Start -->
+<div class="container-fluid position-relative p-0">
+    <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
+        <a href="index.html" class="navbar-brand p-0">
+            <img src="./img/logo.png" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+            aria-label="Toggle navigation">
+            <span class="fa fa-bars"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto py-0">
+                <a href="index.html" class="nav-item nav-link active">Home</a>
+                <a href="about.html" class="nav-item nav-link">About</a>
+                <a href="services.html" class="nav-item nav-link">Services</a>
+                <a href="packages.html" class="nav-item nav-link">Tours</a>
+                <a href="car-hire.html" class="nav-item nav-link">Car Hire</a>
+                <a href="destinations.html" class="nav-item nav-link">Destinations</a>
+                <a href="contact.html" class="nav-item nav-link">Contact</a>
+            </div>
+            <a href="contact.html" class="btn btn-primary rounded-pill py-2 px-4 ms-lg-4">Get a Quote</a>
+        </div>
+    </nav>
