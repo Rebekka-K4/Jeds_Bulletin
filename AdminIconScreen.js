e5d16bb38ad0a13d0f4d3ed2b057e6559dd7f1d4
Nerven-AO <224116428@students.unam.na>
2025-06-03 21:26:01 +0200

    Create AdminIconScreen.js
    chore: import required modules and libraries for AdminIconScreen

---
+++ AdminIconScreen.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Dimensions, Alert, SafeAreaView } from 'react-native';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import * as ImagePicker from 'expo-image-picker';
+import colors from './Colors';
+import { auth } from './firebaseConfig';
+import { useFonts, Poppins_400Regular, Poppins_700Bold } from '@expo-google-fonts/poppins';
+import { useNavigation } from '@react-navigation/native';
+
+import { Ionicons } from '@expo/vector-icons';
+
+const { width } = Dimensions.get('window');
