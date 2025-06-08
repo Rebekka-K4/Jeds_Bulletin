c4ff3aff76313ffe254f43decae0ea0a573c099e
Shetu05 <224150960@students.unam.na>
2025-06-08 21:16:08 +0200

    Update StudentStaffIconScreen.js
    Initial setup of the component, including all required imports such as React, Firebase, navigation, image picker, font loader, and custom configurations.

--- StudentStaffIconScreen.js
+++ StudentStaffIconScreen.js
@@ -1,1 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Dimensions, Alert } from 'react-native';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import * as ImagePicker from 'expo-image-picker';
+import colors from './Colors';
+import { auth } from './firebaseConfig';
+import { useFonts, Poppins_400Regular, Poppins_700Bold } from '@expo-google-fonts/poppins';
+import { useNavigation } from '@react-navigation/native';
+
+const { width } = Dimensions.get('window');
 
